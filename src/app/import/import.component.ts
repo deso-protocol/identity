6a0eb269191a598026c5772a4e97a7b498de0e29
mae beam <maebeam0@gmail.com>
2021-04-30 17:14:13 -0700

    rm comment

--- src/app/import/import.component.ts
+++ src/app/import/import.component.ts
@@ -28,7 +28,6 @@ export class ImportComponent implements OnInit {
           extraText: identity.ExtraText,
           btcDepositAddress: identity.BtcDepositAddress,
           network: identity.Network,
-          // It could be non-standard but we have no way of knowing
         });
       }
 
