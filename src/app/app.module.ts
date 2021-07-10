d7e79c6ea455158d2c5f1f79c9e45e6bee234032
Anna Kutch <annakutch@gmail.com>
2021-07-10 11:06:34 -0400

    prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmbedComponent } from './embed/embed.component';
 import { HomeComponent } from './home/home.component';
-import {FormsModule} from '@angular/forms';
-import {IdentityService} from './identity.service';
-import {CookieModule} from 'ngx-cookie';
+import { FormsModule } from '@angular/forms';
+import { IdentityService } from './identity.service';
+import { CookieModule } from 'ngx-cookie';
 import { LogoutComponent } from './logout/logout.component';
 import { BannerComponent } from './banner/banner.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import {AccountService} from './account.service';
-import {EntropyService} from './entropy.service';
+import { AccountService } from './account.service';
+import { EntropyService } from './entropy.service';
 import { LogInComponent } from './log-in/log-in.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ApproveComponent } from './approve/approve.component';
 import { LogInSeedComponent } from './log-in-seed/log-in-seed.component';
 import { GoogleComponent } from './auth/google/google.component';
@@ -39,13 +38,9 @@ import { AvatarDirective } from './avatar/avatar.directive';
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
-    CookieModule.forRoot()
+    CookieModule.forRoot(),
   ],
-  providers: [
-    IdentityService,
-    EntropyService,
-    AccountService,
-  ],
-  bootstrap: [AppComponent]
+  providers: [IdentityService, EntropyService, AccountService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
