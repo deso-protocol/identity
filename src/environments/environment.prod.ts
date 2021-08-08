84487855970c4885d8e1d684dbd100d8b694c0da
lazynina <lazynina84@gmail.com>
2021-08-07 18:23:20 -0700

    point identity to blue for testing'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hostname: 'identity.bitclout.com',
-  nodeHostname: 'bitclout.green',
+  nodeHostname: 'bitclout.blue',
   fullAccessHostnames: ['bitclout.com', 'bitclout.green', 'bitclout.blue', 'localhost'],
   noAccessHostnames: [''],
   jumioSupported: true,
