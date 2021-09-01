066be8f2618e5d9e7a833b397e206a2a90960a04
lazynina <lazynina84@gmail.com>
2021-09-01 15:48:13 +0300

    fix get referrals USD cents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -60,7 +60,7 @@ export class AppComponent implements OnInit {
       this.backendApiService.GetReferralInfoForReferralHash(referralCode).subscribe((res) => {
         const referralInfo = res.ReferralInfoResponse.Info;
         if (
-          res.IsActive &&
+          res.ReferralInfoResponse.IsActive &&
           (referralInfo.TotalReferrals < referralInfo.MaxReferrals || referralInfo.MaxReferrals == 0)
         ) {
           this.globalVars.referralUSDCents = referralInfo.RefereeAmountUSDCents;
