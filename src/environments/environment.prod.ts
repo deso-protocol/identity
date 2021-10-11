d7c9b8e61d213fb698904686be740ce059761f3c
maebeam <maebeam0@gmail.com>
2021-10-11 14:03:11 -0400

    Revert "enable full access on identity.deso.org (#76)"
    This reverts commit 787ce908d24786f5db3cac6ef0358a5f98161ac9.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   hostname: 'identity.deso.org',
   nodeHostname: 'bitclout.com',
-  fullAccessHostnames: ['bitclout.com', 'bitclout.green', 'bitclout.blue', 'localhost', 'buy.deso.org', 'identity.deso.org'],
+  fullAccessHostnames: ['bitclout.com', 'bitclout.green', 'bitclout.blue', 'localhost', 'buy.deso.org'],
   noAccessHostnames: [''],
   jumioSupported: false,
 };
