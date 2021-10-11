c4f9cc3302a77e1696bc1738f9d1350912f6e17e
maebeam <maebeam0@gmail.com>
2021-10-11 17:59:38 -0400

    Fix origin check

--- src/app/identity.service.ts
+++ src/app/identity.service.ts
@@ -116,12 +116,10 @@ export class IdentityService {
   }
 
   private handleImport(event: MessageEvent) {
-    console.log(event.data.origin);
     // Only allow import events from BitClout Identity
-    if (event.data.origin !== 'https://identity.bitclout.com') {
+    if (event.origin !== 'https://identity.bitclout.com') {
       return;
     }
-    console.log(event.data.origin);
 
     // Import accounnts
     for (const privateUser of Object.values(event.data.payload.privateUsers)) {
