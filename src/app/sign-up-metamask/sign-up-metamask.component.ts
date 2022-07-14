75b933f0bb2e2c461c4bd3c2112411a516132a88
TylerFischer@Deso <98359380+DeSoDog@users.noreply.github.com>
2022-07-14 03:59:38 -0500

    Metamask UI improvements (#147)
    * Button state
    
    * Update to user flow

--- src/app/sign-up-metamask/sign-up-metamask.component.ts
+++ src/app/sign-up-metamask/sign-up-metamask.component.ts
@@ -1,37 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import {ethers} from 'ethers';
-import {BackendAPIService, TransactionSpendingLimitResponse} from '../backend-api.service';
-import {Network} from '../../types/identity';
+import { ethers } from 'ethers';
+import {
+  BackendAPIService,
+  TransactionSpendingLimitResponse,
+} from '../backend-api.service';
+import { Network } from '../../types/identity';
 import HDKey from 'hdkey';
-import {ec} from 'elliptic';
-import {CryptoService} from '../crypto.service';
+import { ec } from 'elliptic';
+import { CryptoService } from '../crypto.service';
 import * as bs58check from 'bs58check';
-import {getSpendingLimitsForMetamask} from '../log-in/log-in.component';
-import {AccountService} from '../account.service';
-import {IdentityService} from '../identity.service';
-import {EntropyService} from '../entropy.service';
-import {GoogleDriveService} from '../google-drive.service';
-import {GlobalVarsService} from '../global-vars.service';
-import {SigningService} from '../signing.service';
-import {Router} from '@angular/router';
-
+import { getSpendingLimitsForMetamask } from '../log-in/log-in.component';
+import { AccountService } from '../account.service';
+import { IdentityService } from '../identity.service';
+import { EntropyService } from '../entropy.service';
+import { GoogleDriveService } from '../google-drive.service';
+import { GlobalVarsService } from '../global-vars.service';
+import { SigningService } from '../signing.service';
+import { Router } from '@angular/router';
+enum SCREEN {
+  CREATE_ACCOUNT = 0,
+  LOADING = 1,
+  ACCOUNT_SUCCESS = 2,
+  AUTHORIZE_MESSAGES = 3,
+  MESSAGES_SUCCESS = 4,
+}
+enum METAMASK {
+  START = 0,
+  CONNECT = 1,
+  ERROR = 2,
+}
 @Component({
   selector: 'app-sign-up-metamask',
   templateUrl: './sign-up-metamask.component.html',
-  styleUrls: ['./sign-up-metamask.component.scss']
+  styleUrls: ['./sign-up-metamask.component.scss'],
 })
 export class SignUpMetamaskComponent implements OnInit {
-  static UNLIMITED_DERIVED_KEY_EXPIRATION = 999999999999;
-
-  SCREEN_CREATE_ACCOUNT = 0;
-  SCREEN_LOADING = 1;
-  SCREEN_ACCOUNT_SUCCESS = 2;
-  SCREEN_AUTHORIZE_MESSAGES = 3;
-  SCREEN_MESSAGES_SUCCESS = 4;
-  currentScreen = this.SCREEN_CREATE_ACCOUNT;
-
-  stopTimeout = false;
-  timeoutTimer = 5;
+  private static UNLIMITED_DERIVED_KEY_EXPIRATION: Readonly<number> = 999999999999;
+  private static TIMER_START_TIME: Readonly<number> = 15;
+  metamaskState: METAMASK = METAMASK.START;
+  currentScreen: SCREEN = SCREEN.CREATE_ACCOUNT;
+  timer: any;
+  SCREEN = SCREEN;
+  METAMASK = METAMASK;
+  timeoutTimer = SignUpMetamaskComponent.TIMER_START_TIME;
   publicKey = '';
 
   constructor(
@@ -44,10 +55,10 @@ export class SignUpMetamaskComponent implements OnInit {
     private backendApi: BackendAPIService,
     private signingService: SigningService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.computeTimeout = this.computeTimeout.bind(this);
+    this.startTimer();
   }
 
   nextStep(): void {
@@ -59,168 +70,141 @@ export class SignUpMetamaskComponent implements OnInit {
    */
 
   launchMetamask(): void {
-    this.signInWithMetamaskNewUser();
-  }
-
-  public connectMetamaskMiddleware(): Promise<any> {
-    return new Promise<any>((resolve, reject) => {
-      this.getProvider()
-        .listAccounts()
-        .then((accounts) => {
-          if (accounts.length === 0) {
-            this.getProvider()
-              .send('eth_requestAccounts', [])
-              .then((res) => {
-                // Metamask successfully connected.
-                resolve(res);
-              })
-              .catch((err) => {
-                // EIP-1193 userRejectedRequest error.
-                if (err.code === 4001) {
-                  console.error(
-                    'user rejected the eth_requestAccounts request'
-                  );
-                } else {
-                  console.error(
-                    'error while sending eth_requestAccounts:',
-                    err
-                  );
-                }
-                reject(err);
-              });
-          } else {
-            resolve(true);
-          }
-        })
-        .catch((err) => {
-          reject(err);
-        });
+    this.signInWithMetamaskNewUser().catch((err) => {
+      console.log('something went wrong with signing in through metamask', err);
+      this.metamaskState = METAMASK.ERROR;
     });
   }
 
-  public verifySignatureAndRecoverAddress(
+  public async connectMetamaskMiddleware(): Promise<boolean> {
+    const accounts = await this.getProvider().listAccounts();
+    if (accounts.length === 0) {
+      return await this.getProvider()
+        .send('eth_requestAccounts', [])
+        .then(() => true)
+        .catch((err) => {
+          // EIP-1193 userRejectedRequest error.
+          if (err.code === 4001) {
+            console.error('user rejected the eth_requestAccounts request');
+          } else {
+            console.error('error while sending eth_requestAccounts:', err);
+          }
+          return false;
+        });
+    }
+    return true;
+  }
+
+  public async verifySignatureAndRecoverAddress(
     message: number[],
     signature: string
-  ): Promise<any> {
+  ): Promise<string> {
     const arrayify = ethers.utils.arrayify;
     const hash = ethers.utils.hashMessage;
     const recoveredAddress = ethers.utils.recoverAddress(
       arrayify(hash(message)),
       signature
     );
-    return new Promise<any>((resolve, reject) => {
-      this.getProvider()
-        .getSigner()
-        .getAddress()
-        .then((publicEthAddress) => {
-          if (recoveredAddress !== publicEthAddress) {
-            reject(
-              'Public key recovered from signature doesn\'t match the signer\'s public key!'
-            );
-          } else {
-            resolve(recoveredAddress);
-          }
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
+    const publicEthAddress = await this.getProvider().getSigner().getAddress();
+    if (recoveredAddress !== publicEthAddress) {
+      throw Error(
+        "Public key recovered from signature doesn't match the signer's public key!"
+      );
+    }
+    return recoveredAddress;
   }
-
   /**
    * Flow for new deso users looking to sign in with metamask
    */
-  public signInWithMetamaskNewUser(): any {
+  public async signInWithMetamaskNewUser(): Promise<any> {
     // generate a random derived key
     const network = this.globalVars.network;
-    const expirationBlock = SignUpMetamaskComponent.UNLIMITED_DERIVED_KEY_EXPIRATION;
+    const expirationBlock =
+      SignUpMetamaskComponent.UNLIMITED_DERIVED_KEY_EXPIRATION;
     const { keychain, mnemonic, derivedPublicKeyBase58Check, derivedKeyPair } =
       this.generateDerivedKey(network);
-    this.connectMetamaskMiddleware()
-      .then(() => {
-        // fetch a spending limit hex string based off of the permissions you're allowing
-        this.backendApi
-          .GetAccessBytes(
-            derivedPublicKeyBase58Check,
-            expirationBlock,
-            getSpendingLimitsForMetamask() as TransactionSpendingLimitResponse
-          )
-          .toPromise()
-          .then((getAccessBytesResponse) => {
-            //  we can now generate the message and sign it
-            this.generateMessageAndSignature(
-              derivedKeyPair,
-              getAccessBytesResponse.AccessBytesHex
-            ).then(({ message, signature }) => {
-              this.currentScreen = this.SCREEN_LOADING;
-              this.verifySignatureAndRecoverAddress(message, signature).then(
-                (publicEthAddress) => {
-                  // TODO: this needs backend's gringotts endpoint implemented.
-                  this.getFundsForNewUsers(signature, message, publicEthAddress)
-                    .then(() => {
-                      // once we have the signature we can fetch the public key from it
-                      const metamaskKeyPair = this.getMetaMaskMasterPublicKeyFromSignature(signature, message);
-                      const metamaskPublicKey = Buffer.from(metamaskKeyPair.getPublic().encode('array', true));
-                      const metamaskPublicKeyHex = metamaskPublicKey.toString('hex');
-                      const metamaskBtcAddress = this.cryptoService.publicKeyToBtcAddress(metamaskPublicKey, Network.mainnet);
-                      const metamaskEthAddress = this.cryptoService.publicKeyToEthAddress(metamaskKeyPair);
-                      const metamaskPublicKeyDeso = this.cryptoService.publicKeyToDeSoPublicKey(metamaskKeyPair, network);
-                      // Slice the '0x' prefix from the signature.
-                      const accessSignature = signature.slice(2);
 
-                      // we now have all the arguments to generate an authorize derived key transaction
-                      this.backendApi
-                        .AuthorizeDerivedKey(
-                          metamaskPublicKeyDeso,
-                          derivedPublicKeyBase58Check,
-                          expirationBlock,
-                          accessSignature,
-                          getAccessBytesResponse.SpendingLimitHex
-                        )
-                        .toPromise()
-                        .then((response) => {
-                          // convert it to a byte array, sign it, submit it
-                          const signedTransactionHex =
-                            this.signingService.signTransaction(
-                              derivedKeyPair.getPrivate().toString('hex'),
-                              response.TransactionHex,
-                              true
-                            );
-                          this.backendApi
-                            .SubmitTransaction(signedTransactionHex)
-                            .toPromise()
-                            .then((res) => {
-                              this.publicKey = this.accountService.addUserWithDepositAddresses(
-                                keychain,
-                                mnemonic,
-                                '',
-                                this.globalVars.network,
-                                metamaskBtcAddress,
-                                metamaskEthAddress,
-                                false,
-                                metamaskPublicKeyHex
-                              );
-                              this.currentScreen = this.SCREEN_ACCOUNT_SUCCESS;
-                              this.computeTimeout();
-                            })
-                            .catch((err) => {
-                              console.error('error', err);
-                            });
-                        })
-                        .catch((err) => {
-                          console.error(err);
-                        });
-                    })
-                    .catch((err) => {
-                      console.error(err);
-                    });
-                }
-              );
-            });
-          });
-      })
-      .catch((err) => {
-        console.error('error connecting Metamask:', err);
+    this.metamaskState = METAMASK.CONNECT;
+    const response = await this.connectMetamaskMiddleware();
+    if (response !== true) {
+      alert('something with wrong with metamask signin');
+    }
+    // fetch a spending limit hex string based off of the permissions you're allowing
+    const getAccessBytesResponse = await this.backendApi
+      .GetAccessBytes(
+        derivedPublicKeyBase58Check,
+        expirationBlock,
+        getSpendingLimitsForMetamask() as TransactionSpendingLimitResponse
+      )
+      .toPromise();
+    //  we can now generate the message and sign it
+    const { message, signature } = await this.generateMessageAndSignature(
+      derivedKeyPair,
+      getAccessBytesResponse.AccessBytesHex
+    );
+
+    const publicEthAddress = await this.verifySignatureAndRecoverAddress(
+      message,
+      signature
+    );
+    // TODO: this needs backend's gringotts endpoint implemented.
+    await this.getFundsForNewUsers(signature, message, publicEthAddress);
+    // once we have the signature we can fetch the public key from it
+    const metamaskKeyPair = this.getMetaMaskMasterPublicKeyFromSignature(
+      signature,
+      message
+    );
+    const metamaskPublicKey = Buffer.from(
+      metamaskKeyPair.getPublic().encode('array', true)
+    );
+    const metamaskPublicKeyHex = metamaskPublicKey.toString('hex');
+    const metamaskBtcAddress = this.cryptoService.publicKeyToBtcAddress(
+      metamaskPublicKey,
+      Network.mainnet
+    );
+    const metamaskEthAddress =
+      this.cryptoService.publicKeyToEthAddress(metamaskKeyPair);
+    const metamaskPublicKeyDeso = this.cryptoService.publicKeyToDeSoPublicKey(
+      metamaskKeyPair,
+      network
+    );
+    // Slice the '0x' prefix from the signature.
+    const accessSignature = signature.slice(2);
+
+    // we now have all the arguments to generate an authorize derived key transaction
+    const authorizeDerivedKeyResponse = await this.backendApi
+      .AuthorizeDerivedKey(
+        metamaskPublicKeyDeso,
+        derivedPublicKeyBase58Check,
+        expirationBlock,
+        accessSignature,
+        getAccessBytesResponse.SpendingLimitHex
+      )
+      .toPromise();
+    // convert it to a byte array, sign it, submit it
+    const signedTransactionHex = this.signingService.signTransaction(
+      derivedKeyPair.getPrivate().toString('hex'),
+      authorizeDerivedKeyResponse.TransactionHex,
+      true
+    );
+
+    this.backendApi
+      .SubmitTransaction(signedTransactionHex)
+      .toPromise()
+      .then((res) => {
+        this.publicKey = this.accountService.addUserWithDepositAddresses(
+          keychain,
+          mnemonic,
+          '',
+          this.globalVars.network,
+          metamaskBtcAddress,
+          metamaskEthAddress,
+          false,
+          metamaskPublicKeyHex
+        );
+        this.currentScreen = this.SCREEN.ACCOUNT_SUCCESS;
+        this.metamaskState = this.METAMASK.START;
+        this.startTimer();
       });
   }
 
@@ -301,7 +285,7 @@ export class SignUpMetamaskComponent implements OnInit {
       (window as any).ethereum
     );
     return provider;
-  }
+  };
 
   public getFundsForNewUsers(
     signature: string,
@@ -342,22 +326,19 @@ export class SignUpMetamaskComponent implements OnInit {
   /**
    * STEP SCREEN_LOADING
    */
-  private computeTimeout(): void {
-    console.log("got here");
-    if (this.stopTimeout) {
-      return;
-    }
-
-    if (this.timeoutTimer <= 0) {
-      this.login();
-      return;
-    }
-
-    this.timeoutTimer--;
-    setTimeout(this.computeTimeout, 1000);
+  private startTimer(): void {
+    this.timer = setInterval(() => {
+      if (this.timeoutTimer === 0) {
+        this.stopTimer();
+        this.login();
+        return;
+      }
+      this.timeoutTimer--;
+    }, 1000);
   }
 
   public login(): void {
+    this.stopTimer();
     this.identityService.login({
       users: this.accountService.getEncryptedUsers(),
       publicKeyAdded: this.publicKey,
@@ -365,14 +346,17 @@ export class SignUpMetamaskComponent implements OnInit {
     });
   }
 
+  public stopTimer(): void {
+    clearInterval(this.timer);
+    this.timeoutTimer = SignUpMetamaskComponent.TIMER_START_TIME;
+  }
+  public continue(): void {
+    this.stopTimer();
+  }
+
   /**
    * STEP SCREEN_ACCOUNT_SUCCESS
    */
-
-  public stopTimer(): void {
-    this.stopTimeout = true;
-    this.timeoutTimer = 5;
-  }
 
   /**
    * STEP SCREEN_AUTHORIZE_MESSAGES
@@ -381,5 +365,4 @@ export class SignUpMetamaskComponent implements OnInit {
   /**
    * STEP SCREEN_MESSAGES_SUCCESS
    */
-
 }
