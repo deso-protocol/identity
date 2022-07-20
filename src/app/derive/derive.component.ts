2608422bf1b5a4aa022c0e86b927d64957a24313
DeSoDog <ty@deso.org>
2022-07-20 14:18:55 -0500

    Spending limits UI cleanup

--- src/app/derive/derive.component.ts
+++ src/app/derive/derive.component.ts
@@ -11,6 +11,8 @@ import { UserProfile } from '../../types/identity';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RouteNames } from '../app-routing.module';
 import { truncatePublicKey } from '../utils';
+import { map, switchMap } from 'rxjs/operators';
+import { of, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-derive',
@@ -28,6 +30,7 @@ export class DeriveComponent implements OnInit {
   derivedPublicKeyBase58Check: string | undefined = undefined;
   expirationDays = 30;
   deleteKey = false;
+  isSingleAccount = false;
 
   constructor(
     private accountService: AccountService,
@@ -43,43 +46,61 @@ export class DeriveComponent implements OnInit {
     // Load profile pictures and usernames
     const publicKeys = this.accountService.getPublicKeys();
     this.hasUsers = publicKeys.length > 0;
-    this.backendApi.GetUserProfiles(publicKeys).subscribe((profiles) => {
-      this.allUsers = profiles;
-    });
 
-    this.activatedRoute.queryParams.subscribe((params) => {
-      if (params.publicKey) {
-        this.publicKeyBase58Check = params.publicKey;
-      }
-      if (params.derivedPublicKey) {
-        this.derivedPublicKeyBase58Check = params.derivedPublicKey;
-      }
-      // We can only revoke if we have both the public key and derived public key from query params.
-      if (
-        params.deleteKey &&
-        this.publicKeyBase58Check &&
-        this.derivedPublicKeyBase58Check
-      ) {
-        this.deleteKey = params.deleteKey === 'true';
-        // We don't want or need to parse transaction spending limit when revoking derived key,
-        // so we initialize a spending limit object with no permissions.
-        this.transactionSpendingLimitResponse = { GlobalDESOLimit: 0 };
-        // Setting expiration days to 0 forces us to have a minimum transaction size that is still valid.
-        this.expirationDays = 0;
-        return;
-      }
-      if (params.transactionSpendingLimitResponse) {
-        this.transactionSpendingLimitResponse = JSON.parse(
-          decodeURIComponent(params.transactionSpendingLimitResponse)
-        );
-      }
-      if (params.expirationDays) {
-        const numDays = parseInt(params.expirationDays, 10);
-        if (numDays > 0) {
-          this.expirationDays = numDays;
+    this.activatedRoute.queryParams
+      .pipe(
+        switchMap((params) => {
+          if (params.publicKey) {
+            this.publicKeyBase58Check = params.publicKey;
+            this.isSingleAccount = true;
+          }
+          if (params.derivedPublicKey) {
+            this.derivedPublicKeyBase58Check = params.derivedPublicKey;
+          }
+          // We can only revoke if we have both the public key and derived public key from query params.
+          if (
+            params.deleteKey &&
+            this.publicKeyBase58Check &&
+            this.derivedPublicKeyBase58Check
+          ) {
+            this.deleteKey = params.deleteKey === 'true';
+            // We don't want or need to parse transaction spending limit when revoking derived key,
+            // so we initialize a spending limit object with no permissions.
+            this.transactionSpendingLimitResponse = { GlobalDESOLimit: 0 };
+            // Setting expiration days to 0 forces us to have a minimum transaction size that is still valid.
+            this.expirationDays = 0;
+            return of(null);
+          }
+          if (params.transactionSpendingLimitResponse) {
+            this.transactionSpendingLimitResponse = JSON.parse(
+              decodeURIComponent(params.transactionSpendingLimitResponse)
+            );
+          }
+          if (params.expirationDays) {
+            const numDays = parseInt(params.expirationDays, 10);
+            if (numDays > 0) {
+              this.expirationDays = numDays;
+            }
+          }
+          return this.backendApi.GetUserProfiles(publicKeys);
+        })
+      )
+      .subscribe((profiles) => {
+        if (profiles) {
+          // if there's an account in the query params display only that
+          if (this.publicKeyBase58Check) {
+            this.allUsers = {
+              [this.publicKeyBase58Check]: profiles[this.publicKeyBase58Check],
+            };
+          } else {
+            // otherwise display all accounts
+            this.allUsers = profiles;
+          }
         }
-      }
-    });
+      });
+
+    // this.backendApi.GetUserProfiles(publicKeys).subscribe((profiles) => {
+    // });
     // Set derive to true
     this.globalVars.derive = true;
   }
