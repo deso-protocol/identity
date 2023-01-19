d7e7aa79224db5780cf8d80eb9cc1d32e3e15a5c
lazynina <lazynina84@gmail.com>
2023-01-18 23:46:59 -0500

    temporarily point testing env identity to another node

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hostname: 'identity.deso.org',
-  nodeURL: 'https://node.deso.org',
+  nodeURL: 'http://rt.deso.run',
   fullAccessHostnames: [
     'bitclout.com',
     'bitclout.green',
