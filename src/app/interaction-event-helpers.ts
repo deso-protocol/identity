f08a6830a12089f934be060e87969883b1a26848
Jackson Dean <jckson.dean@gmail.com>
2023-02-06 13:46:09 -0800

    make sure we don't accidentally log sensitive stuff

--- src/app/interaction-event-helpers.ts
+++ src/app/interaction-event-helpers.ts
@@ -1,7 +1,13 @@
+const SEED_REGEX = /^[0-9a-fA-Z]{64}$/;
+const MNEMONIC_REGEX = /^(?:[a-z]+\s){11}(?:[a-z]+)$/;
+const JWT_REGEX = /^(?:[a-zA-Z0-9_=]+)\.(?:[a-zA-Z0-9_=]+)\.(?:[a-zA-Z0-9_\-\+\/=]*)/;
+
 export const setupInteractionEventListener = () => {
-  window.addEventListener('load', () => logInteractionEvent('window', 'open', null));
+  window.addEventListener('load', () => logInteractionEvent('window', 'open', {}));
   window.addEventListener('beforeunload', () =>
-    logInteractionEvent('window', 'close', null)
+    logInteractionEvent('window', 'close', {
+      url: window.location.href,
+    })
   );
   window.addEventListener(
     'click',
@@ -37,18 +43,31 @@ export const setupInteractionEventListener = () => {
   );
 };
 
+const isSafeValue = (value: string) => {
+  return !(SEED_REGEX.test(value) || MNEMONIC_REGEX.test(value) || JWT_REGEX.test(value));
+}
+
 /**
  * @param object the object that was interacted with, could be a button, a page, a link, a modal, etc.
  * @param event the event that was triggered, could be a click, a hover, a focus, PageView, etc.
  * @param data arbitrary data map that can be used to pass additional information about the interaction.
  */
-export const logInteractionEvent = (object: string, event: string, data: any = {}) => {
+export const logInteractionEvent = (object: string, event: string, data: Record<string,string | number> = {}) => {
   // NOTE: this only works on web apps.
   window.opener?.postMessage(
     {
       category: 'interaction-event',
-      payload: { object, event, data },
+      payload: { object, event, data: sanitizeData(data) },
     },
     '*'
   );
 };
+
+const sanitizeData = (data: Record<string, string | number>) => {
+  return Object.entries(data).reduce((result, [k, v]) => {
+    if (isSafeValue(v.toString())) {
+      result[k] = v;
+    }
+    return result;
+  }, {} as Record<string, string | number>);
+}
