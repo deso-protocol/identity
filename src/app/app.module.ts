d0f9a3c887eb546ca3bd24e439aa773ef06bd353
Jackson Dean <jckson.dean@gmail.com>
2023-07-05 20:58:27 -0700

    replace ngx input thing with the vanilla javascript thing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,59 +1,58 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EmbedComponent } from './embed/embed.component';
-import { HomeComponent } from './home/home.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { IdentityService } from './identity.service';
-import { CookieModule } from 'ngx-cookie';
-import { LogoutComponent } from './logout/logout.component';
-import { BannerComponent } from './banner/banner.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { AccountService } from './account.service';
-import { EntropyService } from './entropy.service';
-import { LogInComponent } from './log-in/log-in.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ApproveComponent } from './approve/approve.component';
-import { LogInSeedComponent } from './log-in-seed/log-in-seed.component';
-import { GoogleComponent } from './auth/google/google.component';
-import { AvatarDirective } from './avatar/avatar.directive';
-import { DeriveComponent } from './derive/derive.component';
-import { JumioComponent } from './jumio/jumio.component';
-import { JumioSuccessComponent } from './jumio/jumio-success/jumio-success.component';
-import { JumioErrorComponent } from './jumio/jumio-error/jumio-error.component';
-import { ErrorCallbackComponent } from './error-callback/error-callback.component';
-import { SharedSecretComponent } from './shared-secret/shared-secret.component';
-import { SignUpGetStarterDESOComponent } from './sign-up-get-starter-deso/sign-up-get-starter-deso.component';
-import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { FreeDesoMessageComponent } from './free-deso-message/free-deso-message.component';
-import { FreeDeSoDisclaimerComponent } from './free-deso-message/free-deso-disclaimer/free-deso-disclaimer.component';
-import { TransactionSpendingLimitComponent } from './transaction-spending-limit/transaction-spending-limit.component';
-import { TransactionSpendingLimitSectionComponent } from './transaction-spending-limit/transaction-spending-limit-section/transaction-spending-limit-section.component';
-import { TransactionSpendingLimitCoinComponent } from './transaction-spending-limit/transaction-spending-limit-coin/transaction-spending-limit-coin.component';
-import { TransactionSpendingLimitNftComponent } from './transaction-spending-limit/transaction-spending-limit-nft/transaction-spending-limit-nft.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CookieModule } from 'ngx-cookie';
 import { SanitizePostBodyPipe } from 'src/lib/pipes/sanitize-and-auto-link-pipe';
 import { SanitizeVideoUrlPipe } from 'src/lib/pipes/sanitize-video-url-pipe';
-import { TransactionSpendingLimitDaoCoinLimitOrderComponent } from './transaction-spending-limit/transaction-spending-limit-dao-coin-limit-order/transaction-spending-limit-dao-coin-limit-order.component';
-import { BuyDeSoComponentWrapper } from './buy-deso/buy-deso/buy-deso.component';
-import { IconsModule } from './icons/icons.module';
-import { GetDesoComponent } from './get-deso/get-deso.component';
+import { TruncateAddressOrUsernamePipe } from 'src/lib/pipes/truncate-deso-address.pipe';
+import { AccountSelectComponent } from './account-select/account-select.component';
+import { AccountService } from './account.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ApproveComponent } from './approve/approve.component';
+import { ArrowToggleComponent } from './arrow-toggle/arrow-toggle.component';
+import { GoogleComponent } from './auth/google/google.component';
+import { AvatarDirective } from './avatar/avatar.directive';
+import { BannerComponent } from './banner/banner.component';
 import { BuyDeSoCompletePageComponent } from './buy-deso/buy-deso-complete-page/buy-deso-complete-page.component';
 import { BuyDesoPageComponent } from './buy-deso/buy-deso-page/buy-deso-page.component';
+import { BuyDeSoComponentWrapper } from './buy-deso/buy-deso/buy-deso.component';
 import { BuyOrSendDesoComponent } from './buy-or-send-deso/buy-or-send-deso.component';
-import { MessagingGroupComponent } from './messaging-group/messaging-group.component';
-import { SignUpMetamaskComponent } from './sign-up-metamask/sign-up-metamask.component';
-import { MetamaskService } from './metamask.service';
-import { ArrowToggleComponent } from './arrow-toggle/arrow-toggle.component';
-import { TruncateAddressOrUsernamePipe } from 'src/lib/pipes/truncate-deso-address.pipe';
+import { DeriveComponent } from './derive/derive.component';
+import { EmbedComponent } from './embed/embed.component';
+import { EntropyService } from './entropy.service';
+import { ErrorCallbackComponent } from './error-callback/error-callback.component';
+import { FreeDeSoDisclaimerComponent } from './free-deso-message/free-deso-disclaimer/free-deso-disclaimer.component';
+import { FreeDesoMessageComponent } from './free-deso-message/free-deso-message.component';
+import { GetDesoComponent } from './get-deso/get-deso.component';
+import { HomeComponent } from './home/home.component';
+import { IconsModule } from './icons/icons.module';
+import { IdentityService } from './identity.service';
+import { JumioErrorComponent } from './jumio/jumio-error/jumio-error.component';
+import { JumioSuccessComponent } from './jumio/jumio-success/jumio-success.component';
+import { JumioComponent } from './jumio/jumio.component';
 import { LogInOptionsComponent } from './log-in-options/log-in-options.component';
-import { AccountSelectComponent } from './account-select/account-select.component';
-import { TransactionSpendingLimitAssociationComponent } from './transaction-spending-limit/transaction-spending-limit-association/transaction-spending-limit-association.component';
-import { TransactionSpendingLimitAccessGroupComponent } from './transaction-spending-limit/transaction-spending-limit-access-group/transaction-spending-limit-access-group.component';
+import { LogInSeedComponent } from './log-in-seed/log-in-seed.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { LogoutComponent } from './logout/logout.component';
+import { MessagingGroupComponent } from './messaging-group/messaging-group.component';
+import { MetamaskService } from './metamask.service';
+import { SharedSecretComponent } from './shared-secret/shared-secret.component';
+import { SignUpGetStarterDESOComponent } from './sign-up-get-starter-deso/sign-up-get-starter-deso.component';
+import { SignUpMetamaskComponent } from './sign-up-metamask/sign-up-metamask.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 import { TransactionSpendingLimitAccessGroupMemberComponent } from './transaction-spending-limit/transaction-spending-limit-access-group-member/transaction-spending-limit-access-group-member.component';
+import { TransactionSpendingLimitAccessGroupComponent } from './transaction-spending-limit/transaction-spending-limit-access-group/transaction-spending-limit-access-group.component';
+import { TransactionSpendingLimitAssociationComponent } from './transaction-spending-limit/transaction-spending-limit-association/transaction-spending-limit-association.component';
+import { TransactionSpendingLimitCoinComponent } from './transaction-spending-limit/transaction-spending-limit-coin/transaction-spending-limit-coin.component';
+import { TransactionSpendingLimitDaoCoinLimitOrderComponent } from './transaction-spending-limit/transaction-spending-limit-dao-coin-limit-order/transaction-spending-limit-dao-coin-limit-order.component';
+import { TransactionSpendingLimitNftComponent } from './transaction-spending-limit/transaction-spending-limit-nft/transaction-spending-limit-nft.component';
+import { TransactionSpendingLimitSectionComponent } from './transaction-spending-limit/transaction-spending-limit-section/transaction-spending-limit-section.component';
+import { TransactionSpendingLimitComponent } from './transaction-spending-limit/transaction-spending-limit.component';
 
 @NgModule({
   declarations: [
@@ -107,7 +106,6 @@ import { TransactionSpendingLimitAccessGroupMemberComponent } from './transactio
     ReactiveFormsModule,
     MatFormFieldModule,
     MatTooltipModule,
-    NgxIntlTelInputModule,
     IconsModule,
     BuyDeSoComponentWrapper,
     CookieModule.forRoot(),
