992ca2e12f6019bbceb1981bc15aa74b208fbd9d
superzordon <zordonclout@gmail.com>
2023-09-06 19:04:20 -0400

    Revert prod node URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hostname: 'identity.deso.org',
-  nodeURL: 'https://z.deso.run',
+  nodeURL: 'https://node.deso.org',
   fullAccessHostnames: [
     'bitclout.com',
     'bitclout.green',
