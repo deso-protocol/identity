6384f1b86ca4ba0be505dab9aec494e85a5a73ca
Jackson Dean <jckson.dean@gmail.com>
2023-09-20 10:04:31 -0700

    fix: fix broken avatar directive

--- src/app/avatar/avatar.directive.ts
+++ src/app/avatar/avatar.directive.ts
@@ -3,6 +3,8 @@ import _ from 'lodash';
 import { BackendAPIService } from '../backend-api.service';
 import { GlobalVarsService } from '../global-vars.service';
 
+const DEFAULT_PROFILE_PIC_URL = window.location.origin + '/assets/placeholder-account-image.png';
+
 @Directive({
   selector: '[appAvatar]',
 })
@@ -22,19 +24,18 @@ export class AvatarDirective implements OnChanges {
 
     if (!this.appAvatar) {
       this.setURLOnElement(
-        this.backendApi.GetDefaultProfilePictureURL(window.location.host)
+        DEFAULT_PROFILE_PIC_URL
       );
       return;
     }
-    // The fallback route is the route to the pic we use if we can't find an avatar for the user.
-    const fallbackRoute = `fallback=${this.backendApi.GetDefaultProfilePictureURL(
-      window.location.host
-    )}`;
 
     // Although it would be hard for an attacker to inject a malformed public key into the app,
     // we do a basic _.escape anyways just to be extra safe.
     const profPicURL = _.escape(
-      this.backendApi.GetSingleProfilePictureURL(this.appAvatar, fallbackRoute)
+      this.backendApi.GetSingleProfilePictureURL(
+        this.appAvatar,
+        DEFAULT_PROFILE_PIC_URL
+      )
     );
 
     // Set the URL on the element.
