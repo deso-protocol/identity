fc5f50a6f5273eacb69e06ec1ec1822f7babca92
Jackson Dean <jckson.dean@gmail.com>
2023-09-19 20:43:23 -0700

    cleanup old code

--- src/app/grouped-account-select/export-seed-dialog/export-seed-dialog.component.ts
+++ src/app/grouped-account-select/export-seed-dialog/export-seed-dialog.component.ts
@@ -14,12 +14,6 @@ export class ExportSeedDialogComponent {
     seedHex: string;
   };
 
-  displayValues?: {
-    mnemonic: string;
-    extraText: string;
-    seedHex: string;
-  };
-
   constructor(
     public dialogRef: MatDialogRef<ExportSeedDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { rootPublicKey: string },
@@ -39,11 +33,6 @@ export class ExportSeedDialogComponent {
       this.data.rootPublicKey
     );
     this.secrets = { mnemonic, extraText, seedHex };
-    this.displayValues = {
-      mnemonic: maskValue(mnemonic),
-      extraText: maskValue(extraText),
-      seedHex: maskValue(seedHex),
-    };
   }
 
   onDisableExport() {
@@ -52,33 +41,4 @@ export class ExportSeedDialogComponent {
     });
     this.dialogRef.close();
   }
-
-  copyValue(value: 'mnemonic' | 'seedHex' | 'extraText') {
-    if (!this.secrets) {
-      throw new Error('secrets are missing.');
-    }
-
-    window.navigator.clipboard.writeText(this.secrets[value]).then(() => {
-      // TODO: set the copy to a success checkmark for a few seconds.
-    });
-  }
-
-  toggleRevealValue(value: 'mnemonic' | 'seedHex' | 'extraText') {
-    if (!(this.secrets && this.displayValues)) {
-      throw new Error('secrets are missing.');
-    }
-
-    const newValue =
-      this.displayValues[value] === this.secrets[value]
-        ? maskValue(this.secrets[value])
-        : this.secrets[value];
-    this.displayValues = {
-      ...this.displayValues,
-      [value]: newValue,
-    };
-  }
 }
-
-const maskValue = (value: string) => {
-  return value.slice().replace(/\S/g, '*');
-};
