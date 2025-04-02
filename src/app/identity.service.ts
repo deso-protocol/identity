6d28768660c326bb677c5f8e252ba9bb2584d3a3
Lazy Nina <lazynina84@gmail.com>
2025-04-02 00:38:43 -0400

    format

--- src/app/identity.service.ts
+++ src/app/identity.service.ts
@@ -606,7 +606,12 @@ export class IdentityService {
     );
 
     if (!hasAccess || !hasEncryptionKey) {
-      console.log("hasAccess: ", hasAccess, "\nhasEncryptionKey:", hasEncryptionKey);
+      console.log(
+        'hasAccess: ',
+        hasAccess,
+        '\nhasEncryptionKey:',
+        hasEncryptionKey
+      );
       this.respond(data.id, { approvalRequired: true });
       return false;
     }
